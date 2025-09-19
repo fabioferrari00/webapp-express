3d37dd90a0644bbf4bc5092fea5f07a9c5292b16
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-09-19 18:01:26 +0200

    [feat] add movieRouter.js and route in app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 
+//importo il file router
+const movieRouter = require("./routers/movieRouter");
+
 
 //definisco la porta su cui far girare l'app
 const port=process.env.PORT;
@@ -13,7 +16,12 @@ app.get("/",(req,res)=>{
     res.send("Rotta base")
 });
 
-//
+//definisco la rotta per i movies
+app.use("/api/movies", movieRouter);
+
+
+
+
 app.listen(port,()=>{
     console.log("server in ascolto")
 })
---
+++ routers/movieRouter.js
@@ -0,0 +1,18 @@
+//importo express
+const express = require("express");
+
+//importo router
+const router = express.Router();
+
+//importo il controller
+const movieController = require("../controllers/movieController");
+
+//definisco le rotte
+//index
+router.get('/',movieController.index);
+
+//show
+router.get('/id',movieController.show);
+
+
+module.exports = router;
